33254ba86d0fa6d8191a89d3d30d0afc7b537dcb
duangsuse <fedora-opensuse@outlook.com>
2018-04-15 19:32:26 +0800

    Partial definition for chunk classes

--- src/main/java/org/duangsuse/tinyaxml/chunk/StartNameSpace.java
+++ src/main/java/org/duangsuse/tinyaxml/chunk/StartNameSpace.java
@@ -2,4 +2,40 @@ package org.duangsuse.tinyaxml.chunk;
 
 import org.duangsuse.tinyaxml.IChunk;
 
-public class StartNameSpace implements IChunk {}
+public class StartNameSpace implements IChunk {
+    int hsize;
+    int type;
+    int size;
+
+    @Override
+    public int getSize() {
+        update();
+        return size;
+    }
+
+    @Override
+    public int getHeaderSize() {
+        update(); // ;-)
+        return hsize;
+    }
+
+    @Override
+    public int getMagic() {
+        return type;
+    }
+
+    // parser
+    @Override
+    public void fromBytes(byte[] bs) {}
+
+    // serializer
+    @Override
+    public byte[] toBytes() {
+        byte[] ret = new byte[size];
+        return ret;
+    }
+
+    // make header inforamtion
+    @Override
+    public void update() {}
+}
--- src/main/java/org/duangsuse/tinyaxml/chunk/StringPool.java
+++ src/main/java/org/duangsuse/tinyaxml/chunk/StringPool.java
@@ -26,11 +26,13 @@ public class StringPool implements IChunk {
 
     @Override
     public int getSize() {
+        update();
         return size;
     }
 
     @Override
     public int getHeaderSize() {
+        update(); // ;-)
         return hsize;
     }
 
@@ -39,15 +41,18 @@ public class StringPool implements IChunk {
         return type;
     }
 
+    // parser
     @Override
     public void fromBytes(byte[] bs) {}
 
+    // serializer
     @Override
     public byte[] toBytes() {
         byte[] ret = new byte[size];
         return ret;
     }
 
+    // make header inforamtion
     @Override
     public void update() {}
 }
